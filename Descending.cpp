a9e78a0c02865ca130bbcafa70cd022489df0032
raldyachmad <87338157+raldyachmad@users.noreply.github.com>
2022-07-21 00:06:42 +0700

    Create Descending.cpp

---
+++ Descending.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+short int maksData, tukar, data[100], temp;
+
+int main() {	
+	// Awal Input
+    cout << "Masukkan MaksData : ";
+    cin >> maksData;
+	
+    for (int i = 0; i < maksData; i++) {
+        cout << "Data ke - " << i + 1 << " = " ;
+        cin >> data[i];
+    }
+    // akhir Input
+	
+	//Awal codingan Selection Sort Descending
+	for (int i = 0; i < maksData+1; i++) {
+        tukar = i;
+        for(int j = i+1; j < maksData; j++){
+            if(data[j] > data[tukar]){
+                tukar = j;
+            }
+        }
+        temp = data[tukar];
+        data[tukar] = data[i];
+        data[i] = temp;
+    }
+    //Akhir codingan Selection Sort Descending
+    
+    //Awal Output
+    cout <<endl;
+		cout <<"Hasil = ";
+	    for(int i = 0; i < maksData; i++){
+	        cout << data[i] << " ";
+	    }
+    //Akhir Output
+    
+    cin.get();
+    return 0;
+}
